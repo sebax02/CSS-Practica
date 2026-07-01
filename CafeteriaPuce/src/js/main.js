3ea5a6031a6c48bdac83f85199a45e908bba1cbf
Estudiante LTIC <Estudiante@ltic.local>
2026-07-01 14:43:27 -0500

    creando la página de servicios

--- CafeteriaPuce/src/js/main.js
+++ CafeteriaPuce/src/js/main.js
@@ -2,7 +2,7 @@ import './style.css'
 import javascriptLogo from './assets/javascript.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import { setupCounter } from '../../counter.js'
 
 document.querySelector('#app').innerHTML = `
 <section id="center">
